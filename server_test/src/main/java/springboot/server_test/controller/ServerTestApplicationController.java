e56e1baa0b6cbea041968ec3f682690af65565a8
Chronoss0518 <sinyusuke5000@gmail.com>
2023-12-26 13:57:36 +0900

    Controllerファイルをcontrollerフォルダへ移行

--- server_test/src/main/java/springboot/server_test/controller/ServerTestApplicationController.java
+++ server_test/src/main/java/springboot/server_test/controller/ServerTestApplicationController.java
@@ -1,4 +1,4 @@
-package springboot.server_test;
+package springboot.server_test.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
